14901fe97a73c6a3d8aeaff6baa4bc0fb05d5afd
Faust <shangguokan@hotmail.com>
2015-04-17 23:28:03 +0200

    add phpunit.xml.dist

---
+++ Tests/Generator/GeneratorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SGK\BarcodeBundle\Tests\Generator;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Class GeneratorTest
+ *
+ * @package SGK\BarcodeBundle\Tests\Generator
+ */
+class GeneratorTest extends PHPUnit_Framework_TestCase
+{
+    public function testGenerate()
+    {
+
+    }
+}
